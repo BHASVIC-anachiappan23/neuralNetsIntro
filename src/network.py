3f7ed3c4cf1741e23841aadc7962dd124273b6e0
BHASVIC-anachiappan23 <145350825+BHASVIC-anachiappan23@users.noreply.github.com>
2025-05-09 18:50:24 +0100

    removingOtherSolutions

--- src/network.py
+++ src/network.py
@@ -129,39 +129,6 @@ class Network(object):
             #(numBiasRows, 1) because of for dnb in zip(nabla_b) -> because of the extra 1 we can filter through each bias or else we would filtering through the whole list
 
         return (sum_nabla_b, nabla_w)
-    def feedforward2(self, a):
-        zs = []
-        activations = [a]
-
-        activation = a
-        for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation) + b
-            zs.append(z)
-            activation = sigmoid(z)
-            activations.append(activation)
-
-        return (zs, activations)
-
-    def different(self, x, y):
-        nabla_b = [0 for i in self.biases]
-        nabla_w = [0 for i in self.weights]
-
-        # feedforward
-        zs, activations = self.feedforward2(x)
-
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
-        nabla_b[-1] = delta.sum(1).reshape([len(delta), 1]) # reshape to (n x 1) matrix
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
-        for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
-            nabla_b[-l] = delta.sum(1).reshape([len(delta), 1]) # reshape to (n x 1) matrix
-            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
-
-        return (nabla_b, nabla_w)
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
